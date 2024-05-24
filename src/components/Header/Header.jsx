e79e9f3d6b131140636c4a5527974d7de43ed259
MariaVan8 <fer_nandita_8@hotmail.com>
2024-05-24 16:43:00 -0700

    Created header structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,30 +5,24 @@ function Header() {
 		<>
 			<header className="header">
 				<div className="header_overlay"></div>
-				<div className="text-layer">
-					<p className="first-layer">Fullstack developer</p>
-					<div className="second-layer">
-						<div className="second-layer_text">Hello,</div>
-						<div className="second-layer_text">I'm</div>
-						<div className="second-layer_text">
-							<span className="accent">Maria </span>Revelo,
+				<div className="header_box">
+					<div className="text-layer">
+						<p className="first-layer">Fullstack Developer</p>
+						<div className="second-layer">
+							<div className="second-layer_text">Hello,</div>
+							<div className="second-layer_text">I'm</div>
+							<div className="second-layer_text">
+								<span className="accent">Maria </span>Revelo,
+							</div>
+							<div className="second-layer_text">fullstack developer</div>
 						</div>
-						<div className="second-layer_text">fullstack developer</div>
 					</div>
-				</div>
-				{/* <div className="header_text-container">
-					<span>Hello,</span>
-					<span>
-						I'm <span className="accent">Maria</span>
-					</span>
-					<span>Revelo</span>
-					<span className="thin">fullstack developer</span>
-				</div>
-				<div className="img-container">
-					<img
-						src={perfil}
-						alt="profile"
-					/>
+					<div className="img-container">
+						<img
+							src={perfil}
+							alt="profile"
+						/>
+					</div>
 				</div>
 				<div className="header_text-wrapper">
 					<p>
@@ -38,14 +32,7 @@ function Header() {
 						<span>adapt and contribute positively</span> to dynamic teams.
 					</p>
 					<button>About me</button>
-				</div> */}
-				{/* <div className="big">
-					<div className="overlay"></div>
-					<div className="text-layer">
-						<p className="first-layer">Fullstack developer</p>
-						<p className="second-layer">Hello</p>
-					</div>
-				</div> */}
+				</div>
 			</header>
 		</>
 	);
