f5990af35c81eeff3b05cb7c6862f9774dafc9bd
MariaVan8 <fer_nandita_8@hotmail.com>
2024-06-28 19:32:41 -0700

    Updated media query for tablet and changed header

--- src/components/Cloud/Cloud.jsx
+++ src/components/Cloud/Cloud.jsx
@@ -1,9 +1,22 @@
 import "./Cloud.scss";
+import aws from "../../assets/icons/aws.png";
+import docker from "../../assets/icons/docker.png";
 
 function Cloud() {
 	return (
 		<>
-			<div className="cloud"></div>
+			<div className="cloud">
+				<div className="cloud_container">
+					<img
+						src={aws}
+						alt="aws"
+					/>
+					<img
+						src={docker}
+						alt="awdockers"
+					/>
+				</div>
+			</div>
 		</>
 	);
 }
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,48 +6,50 @@ import github from "../../assets/icons/github.png";
 function Contact() {
 	return (
 		<>
-			<section className="projects_top">
-				<div className="projects_box">
-					<div className="text-layer">
-						<p className="first-layer">Contact me</p>
-						<div className="second-layer">
-							<div className="second-layer_text">Let's talk about</div>
-							<div className="second-layer_text">
-								your<span className="accent"> new project</span>.
+			<section className="contact_container">
+				<section className="contact_top">
+					<div className="contact_box">
+						<div className="text-layer">
+							<p className="first-layer">Contact me</p>
+							<div className="second-layer">
+								<div className="second-layer_text">Let's talk about</div>
+								<div className="second-layer_text">
+									your<span className="accent"> new project</span>.
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-			</section>
-			<section className="contact">
-				<div className="contact_box">
-					<button>Contact me</button>
-				</div>
-				<div className="contact_socials">
-					<p>Or follow me :</p>
-					<div className="logos">
-						<a
-							href="https://www.linkedin.com/in/maria-revelo"
-							target="_blank">
-							<img
-								src={linkedin}
-								alt="linkedin"
-							/>
-						</a>
-						<a
-							href="https://github.com/MariaVan8"
-							target="_blank">
-							<img
-								src={github}
-								alt="github"
-							/>
-						</a>
+				</section>
+				<section className="contact">
+					<div className="contact_box">
+						<button>Contact me</button>
 					</div>
+					<div className="contact_socials">
+						<p>Or follow me :</p>
+						<div className="logos">
+							<a
+								href="https://www.linkedin.com/in/maria-revelo"
+								target="_blank">
+								<img
+									src={linkedin}
+									alt="linkedin"
+								/>
+							</a>
+							<a
+								href="https://github.com/MariaVan8"
+								target="_blank">
+								<img
+									src={github}
+									alt="github"
+								/>
+							</a>
+						</div>
+					</div>
+				</section>
+				<div className="spacer">
+					<div className="spacer-line"></div>
 				</div>
 			</section>
-			<div className="spacer">
-				<div className="spacer-line"></div>
-			</div>
 		</>
 	);
 }
