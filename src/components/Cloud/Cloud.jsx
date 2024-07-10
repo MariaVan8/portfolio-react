6baf40140918be8ac9eaa3ec8816400323ec151a
MariaVan8 <fer_nandita_8@hotmail.com>
2024-07-09 18:51:10 -0700

    Added desktop media query for database section and cloud section

--- src/components/Cloud/Cloud.jsx
+++ src/components/Cloud/Cloud.jsx
@@ -2,12 +2,17 @@ import "./Cloud.scss";
 import aws from "../../assets/icons/aws.png";
 import docker from "../../assets/icons/docker.png";
 import cloud from "../../assets/icons/cloud.png";
+import azure from "../../assets/icons/azure.png";
 
 function Cloud() {
 	return (
 		<>
 			<div className="cloud">
 				<div className="cloud_container">
+					<img
+						src={azure}
+						alt="azure"
+					/>
 					<img
 						src={aws}
 						alt="aws"
