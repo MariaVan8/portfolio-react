4d7ba07cc61eea0e419d6e06fadb0a0d5fc23d22
MariaVan8 <fer_nandita_8@hotmail.com>
2024-07-18 11:20:29 -0700

    Updated desktop query for soft skills and about me section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -25,16 +25,16 @@ function About() {
 				},
 			},
 			{
-				breakpoint: 1960, // Desktop view
+				breakpoint: 960, // Desktop view
 				settings: {
-					slidesToShow: 4,
+					slidesToShow: 3,
 					centerPadding: "0px", // Adjust this to create the overlap effect
 				},
 			},
 			{
-				breakpoint: 2960, // Desktop view
+				breakpoint: 1960, // Desktop view
 				settings: {
-					slidesToShow: 5,
+					slidesToShow: 2,
 					centerPadding: "0px", // Adjust this to create the overlap effect
 				},
 			},
@@ -61,52 +61,54 @@ function About() {
 					</div>
 				</div>
 			</section>
-			<section className="slider">
-				<Slider {...settings}>
-					<div>
-						<img
-							src={london}
-							alt="Image 1"
-						/>
-					</div>
-					<div>
-						<img
-							src={tower}
-							alt="Image 2"
-						/>
-					</div>
-					<div>
-						<img
-							src={cat}
-							alt="Image 3"
-						/>
-					</div>
-				</Slider>
-			</section>
-			<section className="intro">
-				<p>
-					Hello, I'm Maria. Originally from Ecuador, I bring a rich cultural
-					background and a passion for technology. I am dedicated to making
-					meaningful contributions through my career in software development.
-				</p>
-				<p>
-					I thrive on solving complex problems and continuously learning new
-					skills. My commitment to excellence is reflected in my work ethic and
-					my eagerness to take on new challenges.
-				</p>
-				<p>
-					Outside of work, I find joy in nature and adventure. You'll often find
-					me outdoors, geocaching, or exploring new trails. This love for
-					exploration extends to my travels, where I immerse myself in different
-					cultures and experiences.
-				</p>
-				<p>
-					I am also a devoted cat mom and cherish my Latina heritage, which
-					enriches my life with vibrant traditions and a love for music and
-					dance. These interests keep me grounded and inspire a balanced,
-					fulfilling lifestyle.
-				</p>
-			</section>
+			<div className="about_container">
+				<section className="slider">
+					<Slider {...settings}>
+						<div>
+							<img
+								src={london}
+								alt="Image 1"
+							/>
+						</div>
+						<div>
+							<img
+								src={tower}
+								alt="Image 2"
+							/>
+						</div>
+						<div>
+							<img
+								src={cat}
+								alt="Image 3"
+							/>
+						</div>
+					</Slider>
+				</section>
+				<section className="intro">
+					<p>
+						Hello, I'm Maria. Originally from Ecuador, I bring a rich cultural
+						background and a passion for technology. I am dedicated to making
+						meaningful contributions through my career in software development.
+					</p>
+					<p>
+						I thrive on solving complex problems and continuously learning new
+						skills. My commitment to excellence is reflected in my work ethic
+						and my eagerness to take on new challenges.
+					</p>
+					<p>
+						Outside of work, I find joy in nature and adventure. You'll often
+						find me outdoors, geocaching, or exploring new trails. This love for
+						exploration extends to my travels, where I immerse myself in
+						different cultures and experiences.
+					</p>
+					<p>
+						I am also a devoted cat mom and cherish my Latina heritage, which
+						enriches my life with vibrant traditions and a love for music and
+						dance. These interests keep me grounded and inspire a balanced,
+						fulfilling lifestyle.
+					</p>
+				</section>
+			</div>
 			<div className="spacer">
 				<div className="spacer-line"></div>
 			</div>
