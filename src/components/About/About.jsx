de44761e5e6fb0411c8fae36de0833d172c76871
MariaVan8 <fer_nandita_8@hotmail.com>
2024-07-18 16:54:52 -0700

    Fixed img height in carousel

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -35,7 +35,21 @@ function About() {
 				breakpoint: 1240, // Desktop view
 				settings: {
 					slidesToShow: 2,
-					centerPadding: "80px", // Adjust this to create the overlap effect
+					centerPadding: "0px", // Adjust this to create the overlap effect
+				},
+			},
+			{
+				breakpoint: 1600, // Large desktop view
+				settings: {
+					slidesToShow: 2,
+					centerPadding: "90px",
+				},
+			},
+			{
+				breakpoint: 2600, // Large desktop view
+				settings: {
+					slidesToShow: 4,
+					centerPadding: "90px",
 				},
 			},
 		],
@@ -46,68 +60,71 @@ function About() {
 			<div className="spacer">
 				<div className="spacer-line"></div>
 			</div>
-			<section
-				className="about"
-				id="about">
-				<div className="about_box">
-					<div className="text-layer">
-						<p className="first-layer">Discover more</p>
-						<div className="second-layer">
-							<div className="second-layer_text">Discover more</div>
-							<div className="second-layer_text">
-								<span className="accent">about </span> me.
+			<div className="about_wrapper">
+				<section
+					className="about"
+					id="about">
+					<div className="about_box">
+						<div className="text-layer">
+							<p className="first-layer">Discover more</p>
+							<div className="second-layer">
+								<div className="second-layer_text">Discover more</div>
+								<div className="second-layer_text">
+									<span className="accent">about </span> me.
+								</div>
 							</div>
 						</div>
 					</div>
+				</section>
+				<div className="about_container">
+					<section className="slider">
+						<Slider {...settings}>
+							<div>
+								<img
+									src={london}
+									alt="Image 1"
+								/>
+							</div>
+							<div>
+								<img
+									src={tower}
+									alt="Image 2"
+								/>
+							</div>
+							<div>
+								<img
+									src={cat}
+									alt="Image 3"
+								/>
+							</div>
+						</Slider>
+					</section>
+					<section className="intro">
+						<p>
+							Hello, I'm Maria. Originally from Ecuador, I bring a rich cultural
+							background and a passion for technology. I am dedicated to making
+							meaningful contributions through my career in software
+							development.
+						</p>
+						<p>
+							I thrive on solving complex problems and continuously learning new
+							skills. My commitment to excellence is reflected in my work ethic
+							and my eagerness to take on new challenges.
+						</p>
+						<p>
+							Outside of work, I find joy in nature and adventure. You'll often
+							find me outdoors, geocaching, or exploring new trails. This love
+							for exploration extends to my travels, where I immerse myself in
+							different cultures and experiences.
+						</p>
+						<p>
+							I am also a devoted cat mom and cherish my Latina heritage, which
+							enriches my life with vibrant traditions and a love for music and
+							dance. These interests keep me grounded and inspire a balanced,
+							fulfilling lifestyle.
+						</p>
+					</section>
 				</div>
-			</section>
-			<div className="about_container">
-				<section className="slider">
-					<Slider {...settings}>
-						<div>
-							<img
-								src={london}
-								alt="Image 1"
-							/>
-						</div>
-						<div>
-							<img
-								src={tower}
-								alt="Image 2"
-							/>
-						</div>
-						<div>
-							<img
-								src={cat}
-								alt="Image 3"
-							/>
-						</div>
-					</Slider>
-				</section>
-				<section className="intro">
-					<p>
-						Hello, I'm Maria. Originally from Ecuador, I bring a rich cultural
-						background and a passion for technology. I am dedicated to making
-						meaningful contributions through my career in software development.
-					</p>
-					<p>
-						I thrive on solving complex problems and continuously learning new
-						skills. My commitment to excellence is reflected in my work ethic
-						and my eagerness to take on new challenges.
-					</p>
-					<p>
-						Outside of work, I find joy in nature and adventure. You'll often
-						find me outdoors, geocaching, or exploring new trails. This love for
-						exploration extends to my travels, where I immerse myself in
-						different cultures and experiences.
-					</p>
-					<p>
-						I am also a devoted cat mom and cherish my Latina heritage, which
-						enriches my life with vibrant traditions and a love for music and
-						dance. These interests keep me grounded and inspire a balanced,
-						fulfilling lifestyle.
-					</p>
-				</section>
 			</div>
 			<div className="spacer">
 				<div className="spacer-line"></div>
